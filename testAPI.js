0dbeb9a1b9c6938ab802471e14228be82558eab8
MiquelMontero02 <104500695+MiquelMontero02@users.noreply.github.com>
2025-07-26 11:09:08 +0200

    Update testAPI.js

--- testAPI.js
+++ testAPI.js
@@ -1,5 +1,8 @@
-const tittle=document.querySelector("h1")
-const response=await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-const parsedResponse=await response.json()
-
-tittle.innerText=parsedResponse.name
+async function test(){
+  const tittle=document.querySelector("h1")
+  const response=await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  const parsedResponse=await response.json()
+  
+  tittle.innerText=parsedResponse.name  
+}
+test()
