d3f1c07550ddbdfe1b6e4547c52c918d86d51d3e
MiquelMontero02 <miquelangelmontero@gmail.com>
2026-04-22 09:22:56 +0200

    Apply changes with new components

--- my-vue-app/src/views/HomeView.vue
+++ my-vue-app/src/views/HomeView.vue
@@ -3,9 +3,12 @@
     <h1>Home</h1>
     <p>Mi primera pantalla con Vue y Vite</p>
   </section>
+  <RespositoriSections :username="MiquelMontero02" />
 </template>
 
 <script setup>
+import RespositoriSections from '../components/sections/repositories/respositoriSections.vue';
+
 </script>
 
 <style scoped>
