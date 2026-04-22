6f621f65f917700ccb6c5ae1f419ab5990fa5194
MiquelMontero02 <miquelangelmontero@gmail.com>
2026-04-22 09:21:03 +0200

    Init NavBar Configuration

---
+++ my-vue-app/src/components/navbar/NavBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import NavBarBtn from './NavBarBtn.vue';
+</script>
+
+<template>
+    <div class="navbar">
+        <NavBarBtn v-for="n in 4" :key="n" />
+    </div>
+</template>
+
+<style>
+    .navbar{
+        width: 90dvw;
+        display:flex;
+    }
+</style>
+
---
+++ my-vue-app/src/components/navbar/NavBarBtn.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <a>Textos</a>
+    </div>
+</template>
+
+<style scoped>
+    div{
+        background-color: aquamarine;
+        display: flex;
+        width: 100%;
+        justify-content: center;
+    }
+    button{
+        text-decoration: none;
+    }
+    div:hover{
+        background-color: bisque;
+    }
+</style>
