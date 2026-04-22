edf8ced69ac82d098ac9c7a7c74eda42e2778aaa
MiquelMontero02 <miquelangelmontero@gmail.com>
2026-04-22 09:21:39 +0200

    Init Footer Components

---
+++ my-vue-app/src/components/footer/Footer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    defineProps<{
+        footerText?: string;
+    }>();
+</script>
+
+<template>
+    <footer>
+        <p>&copy; 2026 My Portfolio. All rights reserved.</p>
+        <p v-if="footerText">{{ footerText }}</p>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    display: flex;
+    width: 90dvw;
+    position: sticky;
+    bottom: 0;
+    margin-top: 0.5rem;
+    background-color: aquamarine;
+}
+p{
+    color: #646cff;
+    font-weight: bold;
+}
+</style>
+
+
